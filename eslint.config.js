8cd206fb6a3fb0d3fe6ad0688ae07de8a908d770
Jaime David Daza Tovar <jaimedavid250701@outlook.com>
2026-04-20 13:46:27 +0000

    chore: integrate Prettier + ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default defineConfig([reactHooks.configs.flat.recommended]);
+export default defineConfig([
+  reactHooks.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+]);
